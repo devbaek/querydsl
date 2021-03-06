fb6e236c87a39a8487e359d60cf8af473db4a088
bbaing4564 <bbaing4564@gmail.com>
2021-03-06 17:53:15 +0900

    add example domains and test

---
+++ src/test/java/com/devbaek/querydsl/entity/MemberTest.java
@@ -0,0 +1,54 @@
+package com.devbaek.querydsl.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("test entity")
+    void test_entity() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> lists = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : lists) {
+            System.out.println("member = " + member);
+        }
+
+        // then
+        assertThat(lists.size()).isEqualTo(4);
+    }
+
+}
