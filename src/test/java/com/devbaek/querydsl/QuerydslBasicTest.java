a2634f2f4f583819fc658eca8a017077241e40ef
bky4564 <bky4564@hunet.co.kr>
2021-03-09 08:58:22 +0900

    add sql function test

--- src/test/java/com/devbaek/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/devbaek/querydsl/QuerydslBasicTest.java
@@ -716,4 +716,36 @@ class QuerydslBasicTest {
                 .where(member.age.gt(18))
                 .execute();
     }
+
+    @Test
+    @DisplayName("sql function test")
+    void sqlFunctionTest() {
+        List<String> result = queryFactory
+                .select(
+                        Expressions.stringTemplate(
+                                "function('replace', {0}, {1}, {2})",
+                                member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    @DisplayName("sql function test2")
+    void sqlFunctionTest2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0}", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
